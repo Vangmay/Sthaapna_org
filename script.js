2a8eb2fc71eb21d046b2e97bff2fdbc80a7bd35c
Vangmay Sachan <vincetheutuber@gmail.com>
2021-10-05 10:09:02 +0530

    Few effects

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 parallax = document.getElementById("parallax")
+div1 = document.getElementById("Div1")
 
 window.addEventListener("scroll",function(){
     let offset = window.scrollY*0.5;
-    parallax.style.backgroundPositionY=offset+"px";
+    let text_offset = window.scrollY*-0.8;
+    parallax.style.backgroundPositionY=offset+"px";   
+    div1.style.top=text_offset+"px";   
 })
