29364bff34ac5614b52c114bdbae99efc9fc56ba
Vangmay Sachan <vincetheutuber@gmail.com>
2021-11-21 16:30:03 +0530

    Added div

--- script.js
+++ script.js
@@ -18,18 +18,35 @@ window.addEventListener('scroll',function(){
 
 var count = 3
 var names = [
-    'name1',
-    'name2',
-    'name3',
-    'name4',
-    'name5',
+    'Sharmishtha Singh Ranawat',
+    'Tanisha Acharya',
+    'Moaz Bin Asad',
+    'Raju Kumar',
 ]
 var testimony = [
-    'testimony1',
-    'testimony2',
-    'testimony3',
-    'testimony4',
-    'testimony5'
+    `I am Sharmishtha Singh Ranawat, a third year student of St. Stephen's College, Delhi. I am deeply fond of Indian philosophy, culture and civilizational history. My experience at Sthaapna’s Research conclave was enthralling. The theme of the conclave was unique and very relevant in the current times. It is also an unexplored chapter of India's glorious history which does not find a place in our syllabi. While researching for the conclave, I learnt about some very interesting bits of India's trade and cultural exchanges as well as our historical soft power. There is a lot we can learn from history and work on reviving our past glory to make our lives better. The papers presented by all participants were deeply insightful and were a learning experience. I congratulate Sthaapna for their wonderful initiative which will go a long way in Bharat's larger journey of Atmanirbharta.`
+    ,
+    
+    `"A brave, frank, clean-hearted, courageous and aspiring youth is the only foundation on which the future nation can be built"  These words of Swami Vivekananda are being brought into action by Sthaapna. 
+    Being a part of the Research Conclave organized by Sthaapna has been an experience which is one of a kind. For an aspiring young student like me this has been an enlightening session and has ignited in us the spirit to contribute actively in the resurgence of the rich cultural heritage of India. The deliberation over the need of reviving the lost Indian businesses has paved the way for us, to bring in innovative methods and renew the entrepreneurship skills for the betterment of the society. 
+    As a budding blogger and an enthusiastic debater this has been a wonderful opportunity for me  to learn and express new ideas. It has dawned upon me a new way of thinking and analyzing issues of importance. It has been a privilege to be a part of this event. 
+    `
+    ,
+    
+    `
+    Hi, I'm Moaz bin Asad from Ranchi, Jharkhand.
+    This was the first time when I participated in any Research Conclave. When I went through the topic, "The Lost Indian Businesses", I thought for a quit, nothing was in my mind, neither any idea nor any opinion. But the first time when the topic discussion meeting was conducted, my mind opened up like a fragrance in the air , thousands of thoughts and views, many ideas to show, lots of suggestions on how to take back the lost glory of the golden bird.
+           At last,I would like to thank "Team Sthaapna" for giving me this chance to participate, to read about the history, to know about different ideas, get aware about different lost businesses in different parts of the nation. Actually this conclave strikes directly to the revolutionary thoughts, creates a bit of fire to a dead heart, enlightens a way towards the spirit of True Nationalism..
+    Thank You for giving me this platform and best wishes for the future!`
+    ,
+    
+    `
+    Many a time, problems exist simply because we have inherently drifted away from the solutions in search of something meaningless. 
+    In India thousands such problems exist and the decline of Indigenous business establishments is one of the reasons for that. 
+    The conclave "Resuscitating The Lost Indian Businesses" had everything that one can expect for. 
+    It brought together some really bright young minds. It gave us an opportunity to put forth our ideas and learn from each other. 
+    This conclave can be a milestone for the future of entrepreneurship and cultural rejuvenation in India.
+    `
 ]
 var name_container = document.querySelector('.Name')
 var testimony_container = document.querySelector('.Testimony')
