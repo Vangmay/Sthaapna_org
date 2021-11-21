ee7044ee657abe6b876f6a81ec24a475b2cca8ce
Vangmay Sachan <vincetheutuber@gmail.com>
2021-11-21 15:59:20 +0530

    Added cool div

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ window.addEventListener('scroll',function(){
     video.style.left = offset * -1.2 + "px"
     div3.style.backgroundPositionY = offset * -0.05 + "px"
 })    
+
+var count = 3
+var names = [
+    'name1',
+    'name2',
+    'name3',
+    'name4',
+    'name5',
+]
+var testimony = [
+    'testimony1',
+    'testimony2',
+    'testimony3',
+    'testimony4',
+    'testimony5'
+]
+var name_container = document.querySelector('.Name')
+var testimony_container = document.querySelector('.Testimony')
+
+var counter = 0 
+setInterval(function(){
+    if (counter >= names.length) {
+        counter = 0
+    }
+    name_container.innerHTML = names[counter]
+    testimony_container.innerHTML = testimony[counter]
+    counter = counter + 1;
+    console.log(names.length)
+},3000)
